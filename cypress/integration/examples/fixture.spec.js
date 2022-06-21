54095fb77447acf8ab7b5e03b79b0c58020a24c0
danielapochini <daniela.pochini@bs2tecnologia.com.br>
2022-06-21 18:00:49 -0300

    Fixtures and keyboard simulation

---
+++ cypress/integration/examples/fixture.spec.js
@@ -0,0 +1,17 @@
+describe('Login with fixture Data', () => {
+	it('should try to login', () => {
+		cy.visit('http://zero.webappsecurity.com/login.html')
+
+		cy.fixture('user').then(user => {
+			const username = user.username
+			const password = user.password
+
+			cy.get('#user_login').as('username')
+			cy.get('@username').type(username)
+			cy.get('#user_password').as('password')
+			cy.get('@password').type(password)
+            cy.contains('Sign in').click()
+		})
+	})
+})
+ 
