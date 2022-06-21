6be8670dccba5e5c7a25862565cec38fe9e01fba
danielapochini <daniela.pochini@bs2tecnologia.com.br>
2022-06-21 18:01:29 -0300

    Custom commands

---
+++ cypress/integration/examples/commands.spec.js
@@ -0,0 +1,12 @@
+describe('Keyboard press simulation', () => {
+	it('should login to application using custom commands', () => {
+		cy.visit('http://zero.webappsecurity.com/login.html')
+		
+        cy.fixture('user').then(user => {
+			const username = user.username
+			const password = user.password
+        cy.login(username, password)
+        })
+	})
+})
+ 
